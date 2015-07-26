cc33f51a190da5cc78a48ae64f019adc1a3c7e13
royluo <roy.luoye@hotmail.com>
2015-07-26 18:25:19 +0800

    add db operation
    add db operation

--- stock_trade.py
+++ stock_trade.py
@@ -5,7 +5,7 @@ create by royluo @ 2015.06.29
 
 import urllib2
 import json
-
+import sqlite3
 
 
 end_time_slot = {'09':['35', '40', '45', '50', '55'], \
@@ -22,8 +22,8 @@ def trade_details_get(site):
 	stock_trade = json.loads(html)
 	print stock_trade['begin']
 	print stock_trade['end']
-	for zhubi in stock_trade['zhubi_list']:
-		print "DATE_STR: %s  PRICE: %s VOLUME: %s TURNOVER: %s" % (zhubi["DATE_STR"], zhubi["PRICE"], zhubi["VOLUME_INC"], zhubi["TURNOVER_INC"])
+	return stock_trade['zhubi_list']
+	
 
 
 def trade_site(stock_code):
@@ -33,8 +33,41 @@ def trade_site(stock_code):
 
 
 
+def db_file_create(db_name):
+	return sqlite3.connect(db_name)
 
-site = trade_site("600704")
+def db_table_create(db_connect, table_name):
+	table_create_cmd = "CREATE TABLE " + table_name + " (DATE TEXT PRIMARY KEY  NOT NULL, PRICE REAL, VOLUME INT);"
+	print table_create_cmd
+	db_connect.execute(table_create_cmd)
+	return 0
 
-trade_details_get(site)
 
+def db_item_insert():
+	pass
+
+def db_close(name):
+	pass 	
+
+stock_code = "600704"
+db_file_name = stock_code + ".db"
+db_table_name = "STOCKDATA"
+
+connect = db_file_create(db_file_name)
+#db_table_create(connect, db_table_name)
+
+site = trade_site(stock_code)
+
+zhubi_list = trade_details_get(site)
+for zhubi in zhubi_list:
+	print "DATE_STR: %s  PRICE: %s VOLUME: %s TURNOVER: %s" % (zhubi["DATE_STR"], zhubi["PRICE"], zhubi["VOLUME_INC"], zhubi["TURNOVER_INC"]) 
+	#insert_table_cmd = "INSERT INTO " + db_table_name + " (DATA,PRICE,VOLUME) VALUES (" + zhubi["DATE_STR"] 
+	data = str(zhubi["DATE_STR"])
+	insert_table_cmd = "INSERT INTO " + db_table_name + " (DATE,PRICE,VOLUME) VALUES (" +"\"" + data + "\"" + "," + str(zhubi["PRICE"]) +"," + str(zhubi["VOLUME_INC"]) + ")"
+
+	print insert_table_cmd
+	connect.execute(insert_table_cmd)
+
+connect.commit()
+
+connect.close()
