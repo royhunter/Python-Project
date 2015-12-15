85c2ab7c3aa0c3f544e731be844025fbc0c18fdb
royluo <roy.luoye@hotmail.com>
2015-12-15 23:53:47 +0800

    optimize func

--- stock_trade.py
+++ stock_trade.py
@@ -6,9 +6,11 @@ create by royluo @ 2015.06.29
 import urllib2
 import json
 import sqlite3
+import os
 
+time_key = ['09', '10', '11', '13', '14']
 
-end_time_slot = {'09':['35', '40', '45', '50', '55'], \
+time_map = {'09':['35', '40', '45', '50', '55'], \
 				'10':['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'], \
 				'11':['00', '05', '10', '15', '20', '25'], \
 				'13':['05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'], \
@@ -16,58 +18,88 @@ end_time_slot = {'09':['35', '40', '45', '50', '55'], \
 				}
 
 
-def trade_details_get(site):
+def print_msg(info_string):
+	print "[INFO] " + info_string 
+
+def err_msg(err_string):
+	print "[ERR] " + err_string
+
+def tradeDetailsGet(site):
 	html = urllib2.urlopen(site).read()
 
 	stock_trade = json.loads(html)
-	print stock_trade['begin']
-	print stock_trade['end']
+	print_msg('begin: ' + stock_trade['begin'] + ',' + 'end: ' + stock_trade['end'])
 	return stock_trade['zhubi_list']
 	
 
 
-def trade_site(stock_code):
-	site = "http://quotes.money.163.com/service/zhubi_ajax.html?symbol="+stock_code + "&end=09%3A35%3A00"
-	print site
+def tradeSiteByCode(stock_code, hour, min):
+	site = "http://quotes.money.163.com/service/zhubi_ajax.html?symbol="+stock_code + "&end=" + hour + "%3A" + min + "%3A00"
+	#print site
 	return site
 
 
 
-def db_file_create(db_name):
+def dbFileCreate(db_name):
+	print_msg("db name: " + db_name)
+	if os.path.isfile(db_name) :
+		#print db_name + " has already exist"
+		os.remove(db_name)	
 	return sqlite3.connect(db_name)
 
-def db_table_create(db_connect, table_name):
-	table_create_cmd = "CREATE TABLE " + table_name + " (DATE TEXT PRIMARY KEY  NOT NULL, PRICE REAL, VOLUME INT);"
-	print table_create_cmd
+def dbTableCreate(db_connect, table_name):
+	table_create_cmd = "CREATE TABLE " + table_name + " (TIME CHARACTER(20), PRICE FLOAT, VOLUME_INC INT, TRADE_TYPE INT);"
+	#print table_create_cmd
 	db_connect.execute(table_create_cmd)
-	return 0
+	print_msg(table_name + " create successfully")
+	return
 
 
-def db_item_insert():
-	pass
+def dbItemInsert(zhubi_list, db_conn, db_table_name):
+	for zhubi in zhubi_list:
+		#print "DATE_STR: %s  PRICE: %s VOLUME_INC: %s " % (zhubi["DATE_STR"], zhubi["PRICE"], zhubi["VOLUME_INC"])
+		#data = zhubi["DATE_STR"]
+		insert_table_cmd = "INSERT INTO " + db_table_name + " (TIME, PRICE, VOLUME_INC) VALUES (" +"\"" + zhubi["DATE_STR"] + "\"" + "," + str(zhubi["PRICE"]) +"," + str(zhubi["VOLUME_INC"]) + ")"
+		#print insert_table_cmd
+		db_conn.execute(insert_table_cmd)
+	db_conn.commit()
+
 
 def db_close(name):
-	pass 	
+	name.close() 	
+
+
+def tradeItemStore(stock_code, db_connect, db_table_name):
+	for k in time_key:
+		for v in time_map[k]:
+			site = tradeSiteByCode(stock_code, k, v)
+			zhubi_list = tradeDetailsGet(site)
+			dbItemInsert(zhubi_list, db_connect, db_table_name)
+
+
+def tradeDataStore(stock_code):
+	db_name = stock_code + '.db'
+	table_name = 'TRADE_TABLE'
+	db_connect = dbFileCreate(db_name)
+	dbTableCreate(db_connect, table_name)
+	tradeItemStore(stock_code, db_connect, table_name)
+	db_close(db_connect)
+
 
 stock_code = "600704"
-db_file_name = stock_code + ".db"
-db_table_name = "STOCKDATA"
+tradeDataStore(stock_code)
 
-connect = db_file_create(db_file_name)
-#db_table_create(connect, db_table_name)
 
-site = trade_site(stock_code)
+def tradeInfoQuery(stock_code):
+	db_name = stock_code + '.db'
+	db_table_name = 'TRADE_TABLE'
+	if not os.path.isfile(db_name):
+		err_msg(db_name + "is not exist")
+		return
+	db_connect = sqlite3.connect(db_name)	
+	cursor = db_connect.execute("SELECT TIME, PRICE, VOLUME_INC from " + db_table_name)	
+	for row in cursor:
+		print "time: %s, price %f, volume %s" %(row[0], row[1], row[2])
 
-zhubi_list = trade_details_get(site)
-for zhubi in zhubi_list:
-	print "DATE_STR: %s  PRICE: %s VOLUME: %s TURNOVER: %s" % (zhubi["DATE_STR"], zhubi["PRICE"], zhubi["VOLUME_INC"], zhubi["TURNOVER_INC"]) 
-	#insert_table_cmd = "INSERT INTO " + db_table_name + " (DATA,PRICE,VOLUME) VALUES (" + zhubi["DATE_STR"] 
-	data = str(zhubi["DATE_STR"])
-	insert_table_cmd = "INSERT INTO " + db_table_name + " (DATE,PRICE,VOLUME) VALUES (" +"\"" + data + "\"" + "," + str(zhubi["PRICE"]) +"," + str(zhubi["VOLUME_INC"]) + ")"
 
-	print insert_table_cmd
-	connect.execute(insert_table_cmd)
-
-connect.commit()
-
-connect.close()
+tradeInfoQuery(stock_code)
